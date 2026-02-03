4158f71fbd413656c45af9c069fffd051f2befb1
snigdha-satva <snigdha@satvasolutions.com>
2026-02-03 09:54:10 +0530

    Changed the type to module and implemented the related changes in all the files

--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -1,13 +1,16 @@
-const express = require("express");
-const { getAllNotes, updateNote, deleteNote, createNote } = require("../controllers/notes.Controller");
+import express from "express";
+import {
+  getAllNotes,
+  updateNote,
+  deleteNote,
+  createNote
+} from "../controllers/notes.Controller.js";
+
 const router = express.Router();
 
 router.get("/", getAllNotes);
-
 router.post("/", createNote);
-
 router.put("/:id", updateNote);
-
 router.delete("/:id", deleteNote);
 
-module.exports = router;
+export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,11 @@
-// import express from 'express';
-const express = require('express');
-const notesRoutes = require('./routes/notesRoutes.js');
+import express from 'express';
+import notesRoutes from "./routes/notesRoutes.js";
+
+// const express = require('express');
+// const notesRoutes = require('./routes/notesRoutes.js');
 const app = express();
 
 app.use("/api/notes", notesRoutes)
-
-
 
 
 app.listen(5001, () => {
